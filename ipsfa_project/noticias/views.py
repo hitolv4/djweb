9bdf06cda63b14a539f2021c4ccad96011cf79a7
victor <victorravelolv4@gmail.com>
2016-12-16 12:21:49 -0400

    tercer commit

--- ipsfa_project/noticias/views.py
+++ ipsfa_project/noticias/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Nota
 
 
 def lista_publicaciones(request):
-    return render(request, 'noticias/lista_publicaciones.html')
+    posts = Nota.objects.filter(
+        fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'noticias/lista_publicaciones.html', {'posts': posts})
