7ca94ef7291c85e6850281daa23b81c2b5645eaa
victor <victorravelolv4@gmail.com>
2016-12-19 13:45:56 -0400

    base html y css

--- ipsfa_project/noticias/urls.py
+++ ipsfa_project/noticias/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.lista_publicaciones),
+    url(r'^$', views.lista_publicaciones, name='lista_publicaciones'),
+    #url(r'^nota/(?P<pk>[0-9]+)/$', views.detalle, name='detalle'),
+    url(r'^nota/(?P<pk>\d+)/$', views.detalle, name='detalle')
 ]
--- ipsfa_project/noticias/views.py
+++ ipsfa_project/noticias/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Nota
 
@@ -7,3 +7,8 @@ def lista_publicaciones(request):
     posts = Nota.objects.filter(
         fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'noticias/lista_publicaciones.html', {'posts': posts})
+
+
+def detalle(request, pk):
+    post = get_object_or_404(Nota, pk=pk)
+    return render(request, 'noticias/detalle.html', {'post': post})
